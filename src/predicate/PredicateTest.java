c7dd26a966403e5f3394162953bbc7b560e3682f
dougdasilva <douglas_wfs@hotmail.com>
2025-08-09 19:30:44 -0300

    fix predicate

--- src/predicate/PredicateTest.java
+++ src/predicate/PredicateTest.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 public class PredicateTest {
 
     public static void main(String[] args) {
-        Predicate<Product> isHigh = prod -> prod.discount <= 0.04d;
+        Predicate<Product> isHigh = prod -> prod.discount <= 0.04d || prod.discount >= 0.1d;
         Product product = new Product("Chocolate",4d,0.05d);
 
         System.out.println(isHigh.test(product));
